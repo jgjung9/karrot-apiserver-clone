158db6ea5a0df4eaa116961f54a4049483c746a4
gio <wnsgyo9@gmail.com>
2025-08-17 05:57:28 +0900

    GlobalExceptionHandler 로그 변경

--- src/main/java/karrot/chat/apiserver/exception/GlobalExceptionHandler.java
+++ src/main/java/karrot/chat/apiserver/exception/GlobalExceptionHandler.java
@@ -12,7 +12,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ApiResponse<Void> handleGenericException(Exception e) {
-      log.error(e.getMessage());
+      log.error(e.getMessage(), e);
       return ApiResponse.internalServerError("서버에 오류가 발생했습니다. 잠시 후 다시 시도해주세요");
     }
 }
